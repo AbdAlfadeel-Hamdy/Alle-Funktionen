63ed47c16033607f4dea9ed3ebdff59ab424815a
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-08-26 03:00:17 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SignIn from "./LoginPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  // const submitLoginHandler = (email: string, password: string) => {
+  //   if(email === "")
+  //   console.log(email, password);
+  // };
+  // return <SignIn onSubmit={submitLoginHandler} />;
+  return <SignIn />;
+};
 
 export default App;
